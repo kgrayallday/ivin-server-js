c8c2c1aca645236fc69609b112c620b13b60d15a
kgrayallday <kylemaxgray@gmail.com>
2022-03-08 15:16:12 -0800

    add todo notes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 
+// Home - where you enter the ivin.app website
 app.get('/', (req, res) => {
-  res.send('Sup World?!')
+  res.send('Sup World?!');
+  // will want to render home res.render('home');
 });
+
+// create login/admin dash
+// app.get('admin', (req, res) => {
+// res.render('admin');
+// });
+//
+// TODO: how to connect the android application to the js server
 
 app.listen(port, () => {
   console.log(`Ivin server is listening on port ${port}`)
