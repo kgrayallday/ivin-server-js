edafb61942e1d1edc4144473b67a318bf51bee86
Kyle Gray <kylemaxgray@gmail.com>
2022-03-09 23:23:42 -0800

    add some example code and comments

--- server.js
+++ server.js
@@ -5,17 +5,23 @@ const port = process.env.PORT || 3000;
 
 // Home - where you enter the ivin.app website
 app.get('/', (req, res) => {
-  res.send('Sup World?!');
+  res.send('This is main page!');
   // will want to render home res.render('home');
 });
 
+app.get('/dbi', (req, res) => {
+  res.send('Inserting into Database...');
+});
 // create login/admin dash
 // app.get('admin', (req, res) => {
 // res.render('admin');
 // });
 //
 // TODO: how to connect the android application to the js server
-
+// app.post('/dbi', (req, res) => {
+//   if...
+// )};
+//
 app.listen(port, () => {
   console.log(`Ivin server is listening on port ${port}`)
 })
